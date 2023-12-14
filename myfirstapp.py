7942628acf161e2b049aa4476cb7e5b1d53f6e79
redbebird <153788516+redbebird@users.noreply.github.com>
2023-12-14 11:35:33 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -35,8 +35,8 @@ elif option=='Long Process':
     latest_iteration = st.empty()
     bar = st.progress(0)
 
-    for i in range(1000):
-        latest_iteration.text(f'Iteration {i+5}')
+    for i in range(100):
+        latest_iteration.text(f'Iteration {i+1}')
         bar.progress(i + 1)
         time.sleep(0.5)
 
