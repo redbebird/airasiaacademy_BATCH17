148c39ac2a299587cb7e329a2fadc4bae5a70144
redbebird <153788516+redbebird@users.noreply.github.com>
2023-12-14 11:17:06 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -3,7 +3,11 @@ import numpy as np
 import pandas as pd
 
 st.header("My very first lovely Streamlit App")
-st.write(pd.DataFrame({
+
+st.write('Before you continue, please read the [Terms and Conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+show = st.checkbox('I shall agree the terms and conditions')
+if show:
+    st.write(pd.DataFrame({
     'Students': ['John', 'Lofa', 'Siti', 'Amy'],
     'Attendance Status': ['yes', 'yes', 'yes', 'no']
-}))
+    }))
