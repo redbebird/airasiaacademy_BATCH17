6cf9962aa5d73b5ae3a69eae17b9f4ed77a3a08a
redbebird <153788516+redbebird@users.noreply.github.com>
2023-12-14 11:06:13 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.header("My first Streamlit App")
+st.header("My very first lovely Streamlit App")
 st.write(pd.DataFrame({
     'Students': ['John', 'Lofa', 'Siti', 'Amy'],
     'Attendance Status': ['yes', 'yes', 'yes', 'no']
