c0048133106167026bbc876acdacbb2be6343e83
redbebird <153788516+redbebird@users.noreply.github.com>
2023-12-14 14:21:59 +0800

    Update advertising.py

--- advertising.py
+++ advertising.py
@@ -14,11 +14,9 @@ def user_input_features():
     TV = st.sidebar.slider('TV', 4.3, 7.9, 5.4)
     Radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
     Newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
-    Sales = st.sidebar.slider('Sales', 0.1, 2.5, 0.2)
     data = {'TV': TV,
             'Radio': Radio,
-            'Newspaper': Newspaper,
-            'Sales': Sales}
+            'Newspaper': Newspaper}
     features = pd.data(data, index=[0])
     return features
 
